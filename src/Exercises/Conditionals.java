cd6cf043369f2b41bc6c87be68481a6326ce758e
anmyla <myla.ariaga@gmail.com>
2023-03-01 20:51:29 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/Exercises/Conditionals.java

--- src/Exercises/Conditionals.java
+++ src/Exercises/Conditionals.java
@@ -1,26 +1,14 @@
 package Exercises;
-
-import java.util.Scanner;
-
 public class Conditionals {
+    //Solutions to the exercises- done with the whole class
 
     public static void main(String[] args) {
-
         System.out.println("Senior, Student, Pet Discount");
-
         checkIfDiscount(false, false, false);
         checkIfDiscount(false, false, true);
         checkIfDiscount(false, true, true);
         checkIfDiscount(true, true, true);
 
-    /*    Scanner scanner1 = new Scanner(System.in);
-        System.out.println( "How many non fiction books you bought? ");
-        int nonFiction = scanner1.nextInt();
-
-        Scanner scanner2 = new Scanner(System.in);
-        System.out.println( "How many fiction books you bought? ");
-        int fiction = scanner2.nextInt();
-    */
         System.out.println();
         System.out.println("Bookstore discount");
         checkDiscountBooks(2, 0);
@@ -31,13 +19,8 @@ public class Conditionals {
         checkDiscountBooks(5, 0);
         checkDiscountBooks(4, 5);
         checkDiscountBooks(5, 3);
-
-
     }
-
     public static void checkIfDiscount(boolean isSenior, boolean isStudent, boolean isDog) {
-        // First Conditional - the class solution
-
         if (isSenior) {
             System.out.println("Discount!");
         } else if (isDog) {
@@ -48,9 +31,7 @@ public class Conditionals {
             System.out.println("No discount!");
         }
     }
-
     public static void checkDiscountBooks(int nonFiction, int fiction) {
-
         if (nonFiction > 2 && fiction > 1) {
             System.out.println("Discount!");
         } else {
