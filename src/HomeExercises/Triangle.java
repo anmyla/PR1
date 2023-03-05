e2db330ad1cfdd5fbaa3370ad7181389ffffebba
anmyla <myla.ariaga@gmail.com>
2023-03-05 21:53:21 +0100

    21:48,  5.march

--- src/HomeExercises/Triangle.java
+++ src/HomeExercises/Triangle.java
@@ -3,9 +3,9 @@ package HomeExercises;
 public class Triangle {
     public static void main(String[] args) {
         int x = 10;
-        for (int i = 1; i <= x; ++i) {
-            for (int j = 1; j < i; ++j) {
-                System.out.print(j);
+        for (int i = 1; i < x; ++i) {
+            for (int y = 1; y < i; ++y) {
+                System.out.print(y);
             }
             System.out.println(i);
         }
