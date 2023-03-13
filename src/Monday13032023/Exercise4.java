7b549db2e0f0484c30d759848353da2b634710d4
anmyla <myla.ariaga@gmail.com>
2023-03-13 15:23:07 +0100

    15:22 13032023

--- src/Monday13032023/Exercise4.java
+++ src/Monday13032023/Exercise4.java
@@ -4,48 +4,107 @@ import java.util.Scanner;
 
 public class Exercise4 {
     public static void main(String[] args) {
+
+//        double penalty = penalty(penalty);
+//        System.out.println("The return value of penalty is: " + penalty);
+//
+//        double discount = discount(penalty);
+//        System.out.println("the total amount of penalty is: " +penalty );
+
+    }
+
+    public static double penalty(double penalty) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter Speed: ");
         int speed = scanner.nextInt();
 
         int overSpeed = speed - 50;
+        penalty = 0;
 
-        if (overSpeed > 0) {
-            System.out.println(overSpeed + " km/h more than the allowed 50km/h");
-            if ((overSpeed > 0) && (overSpeed <= 10)) {
-                System.out.println("Written Warning!");
-            } else if ((overSpeed > 10) && (overSpeed < 20)) {
-                System.out.println("Penalty: 30 Euro");
-            } else if ((overSpeed > 20) && (overSpeed < 30)) {
-                System.out.println("Penalty: 50 Euro");
-            } else if ((overSpeed > 30) && (overSpeed < 50)) {
-                System.out.println("Penalty: 100 Euro");
-            } else if ((overSpeed > 50) && (overSpeed < 100)) {
-                System.out.println("Penalty: 500 Euro");
-            } else {
-                System.out.println("Penalty: 1500 Euro");
-            }
-        } else {
+        if (overSpeed < 0) {
             System.out.println("You're a good driver!");
+        } else if ((overSpeed > 0) && (overSpeed <= 10)) {
+            System.out.println("You are driving " + overSpeed + " km/h more than allowed. You will get a written warning!");
+        } else if (overSpeed <= 20) {
+            System.out.println("You are driving " + overSpeed + " km/h more than allowed. You should pay the penalty: 30 Euro");
+            penalty = penalty + 30;
+        } else if (overSpeed <= 30) {
+            System.out.println("You are driving " + overSpeed + " km/h more than allowed. You should pay the penalty: 50 Euro");
+            penalty = penalty + 50;
+        } else if (overSpeed <= 50) {
+            System.out.println("You are driving " + overSpeed + " km/h more than allowed. You should pay the penalty: 100 Euro");
+            penalty = penalty + 100;
+        } else if (overSpeed <= 100) {
+            System.out.println("You are driving " + overSpeed + " km/h more than allowed. You should pay the penalty: 500 Euro");
+            penalty = penalty + 500;
+        } else {
+            System.out.println("You are driving " + overSpeed + " km/h more than allowed. You should pay the penalty: 1500 Euro");
+            penalty = penalty + 1500;
         }
 
-        //---------------------------------------------
         System.out.println("Enter alcohol level: ");
         double alcoholLevel = scanner.nextDouble();
 
-        if (alcoholLevel > 0) {
-            if (alcoholLevel > 0 && alcoholLevel <= 0.5) {
-                System.out.println("Everything's fine!");
-            } else if (alcoholLevel > 0.5 && alcoholLevel <= 1.0) {
-                System.out.println("Penalty: 50 Euro");
-            } else if (alcoholLevel > 1.0 && alcoholLevel <= 2.0) {
-                System.out.println("Penalty: 100 Euro");
-            } else {
-                System.out.println("Penalty: 2500 Euro");
-            }
-        } else {
+        if (alcoholLevel < 0) {
             System.out.println("No alcohol in your system!");
+        } else if (alcoholLevel <= 0.5) {
+            System.out.println("Alcohol level is within tolerance.");
+        } else if (alcoholLevel <= 1.0) {
+            System.out.println("Your alcohol level is: " + alcoholLevel + "pro/mille. Your penalty is 50 Euro");
+            penalty = penalty + 50;
+        } else if (alcoholLevel <= 2.0) {
+            System.out.println("Your alcohol level is: " + alcoholLevel + "pro/mille. Your penalty is 100 Euro");
+            penalty = penalty + 100;
+        } else {
+            System.out.println("Your alcohol level is: " + alcoholLevel + "pro/mille. Your penalty is 2500 Euro");
+            penalty = penalty + 2500;
         }
 
+        return penalty;
+    }
+
+    public static double discount(double penalty) {
+        if (penalty > 0) {
+            System.out.println("Your total penalty is: " + penalty + "Euro");
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter Month: ");
+            String month = scanner.next();
+
+            switch (month) {
+                case "January":
+                case "February":
+                    System.out.println("It is " + month + ". No discount possible! Your total penalty is " + penalty + " Euro.");
+                    break;
+                case "March":
+                case "April":
+                    penalty = penalty * 0.90;
+                    System.out.println("It is " + month + ". Your total penalty is " + penalty + " Euro.");
+                    break;
+                case "May":
+                case "June":
+                    penalty = penalty * 0.85;
+                    System.out.println("It is " + month + ". Your total penalty is " + penalty + " Euro.");
+                    break;
+                case "July":
+                case "August":
+                    penalty = penalty * 0.80;
+                    System.out.println("It is " + month + ". Your total penalty is " + penalty + " Euro.");
+                    break;
+                case "September":
+                case "October":
+                    penalty = penalty * 0.75;
+                    System.out.println("It is " + month + ". Your total penalty is " + penalty + " Euro.");
+                    break;
+                case "November":
+                case "December":
+                    penalty = penalty * 0.70;
+                    System.out.println("It is " + month + ". Your total penalty is " + penalty + " Euro.");
+                    break;
+            }
+        } else {
+            System.out.println(" Everything's fine!");
+        }
+        return penalty;
     }
 }
---
+++ src/Monday13032023/Exercise6.java
@@ -0,0 +1,42 @@
+package Monday13032023;
+
+import java.util.Scanner;
+
+public class Exercise6 {
+    public static void main(String[] args) {
+//        daysOfTheMonth();
+//        int numberOfDays = daysOfTheMonth();
+//        System.out.println("number of days from the method daysOfTheMonth: " + numberOfDays);
+    }
+
+    public static int daysOfTheMonth() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a month: ");
+        String month = scanner.next();
+        int days = 0;
+
+        switch (month.toLowerCase()) {
+            case "january":
+            case "march":
+            case "may":
+            case "july":
+            case "august":
+            case "october":
+            case "december":
+                System.out.println(month + " has 31 days");
+                days = days + 31;
+                break;
+            case "april":
+            case "june":
+            case "september":
+            case "november":
+                System.out.println(month + " has 30 days");
+                days = days + 30;
+                break;
+            case "february":
+                System.out.println(month + " has 28 days");
+                days = days + 28;
+        }
+        return days;
+    }
+}
