bd7f5897386bbde2587627806a86f5c8ffcc91d3
anmyla <myla.ariaga@gmail.com>
2023-03-14 19:21:06 +0100

    19:20,  14.march

--- src/ExercisesPart2/Bankomat.java
+++ src/ExercisesPart2/Bankomat.java
@@ -4,32 +4,29 @@ import java.util.Scanner;
 
 public class Bankomat {
     public static void main(String[] args) {
-        Scanner input1 = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
         System.out.println("Please enter your balance: " );
-        double balance = input1.nextDouble();
+        double balance = scanner1.nextDouble();
 
+        int choice = 0;
+        System.out.println("Please enter your choice if you wish to make another transaction: " );
         System.out.println("Menu: \n" +
                 "1. Deposit \n" +
                 "2. Withdraw \n" +
                 "3. Check Balance \n" +
                 "4. Exit" );
-
-        int choice;
         do {
-            Scanner input2 = new Scanner(System.in);
+            Scanner scanner3 = new Scanner(System.in);
             System.out.println("Enter your choice: " );
-            choice = input1.nextInt();
+            choice = scanner3.nextInt();
         } while (choice < 1 || choice > 4);
-
 
         do {
             switch (choice) {
-
-
                 case 1:
-                    Scanner input2 = new Scanner(System.in);
+                    Scanner input1 = new Scanner(System.in);
                     System.out.println("Enter the amount you wish to deposit: " );
-                    double deposit = input2.nextDouble();
+                    double deposit = input1.nextDouble();
                     balance = balance + deposit;
                     System.out.println("Your current balance is: " + balance + " Euro." );
                     System.out.println("Please enter your choice if you wish to make another transaction: " );
@@ -40,18 +37,18 @@ public class Bankomat {
                             "4. Exit" );
                     do {
                         System.out.println("Enter your choice: " );
-                        choice = input2.nextInt();
+                        choice = input1.nextInt();
                     } while (choice < 1 || choice > 4);
-                    break;
 
+                    break;
                 case 2:
-                    Scanner input3 = new Scanner(System.in);
+                    Scanner input2 = new Scanner(System.in);
                     System.out.println("Enter the amount you wish to withdraw: " );
-                    double withdraw = input3.nextDouble();
+                    double withdraw = input2.nextDouble();
                     while (balance < withdraw) {
                         System.out.println("Insufficient Balance! Your available balance is only: " + balance + " Euro." );
                         System.out.println("Enter the amount you wish to withdraw: " );
-                        withdraw = input3.nextDouble();
+                        withdraw = input2.nextDouble();
                     }
                     balance = balance - withdraw;
                     System.out.println("Withdrawal successful! Your remaining balance is:" + balance + " Euro." );
@@ -63,12 +60,12 @@ public class Bankomat {
                             "4. Exit" );
                     do {
                         System.out.println("Enter your choice: " );
-                        choice = input3.nextInt();
+                        choice = input2.nextInt();
                     } while (choice < 1 || choice > 4);
-                    break;
 
+                    break;
                 case 3:
-                    Scanner input4 = new Scanner(System.in);
+                    Scanner input3 = new Scanner(System.in);
                     System.out.println("Your current balance is: " + balance + " Euro." );
                     System.out.println("Please enter your choice if you wish to make another transaction: " );
                     System.out.println("Menu: \n" +
@@ -78,12 +75,28 @@ public class Bankomat {
                             "4. Exit" );
                     do {
                         System.out.println("Enter your choice: " );
-                        choice = input4.nextInt();
+                        choice = input3.nextInt();
                     } while (choice < 1 || choice > 4);
-                    break;
 
+                    break;
             }
         } while (choice < 4) ;
         System.out.println("Thanks for banking with us! Goodbye!");
+    }
+
+    public static int menu (){
+        int choice = 0;
+        System.out.println("Please enter your choice if you wish to make another transaction: " );
+        System.out.println("Menu: \n" +
+                "1. Deposit \n" +
+                "2. Withdraw \n" +
+                "3. Check Balance \n" +
+                "4. Exit" );
+        do {
+            Scanner scanner2 = new Scanner(System.in);
+            System.out.println("Enter your choice: " );
+            choice = scanner2.nextInt();
+        } while (choice < 1 || choice > 4);
+        return choice;
     }
 }
--- src/Monday13032023/Exercise4.java
+++ src/Monday13032023/Exercise4.java
@@ -5,45 +5,49 @@ import java.util.Scanner;
 public class Exercise4 {
     public static void main(String[] args) {
 
-//        double penalty = penalty(penalty);
-//        System.out.println("The return value of penalty is: " + penalty);
-//
-//        double discount = discount(penalty);
-//        System.out.println("the total amount of penalty is: " +penalty );
-
+    penaltyForOverSpeeding(100);
+    penaltyForDrinking(2500);
+    discount("December");
     }
 
-    public static double penalty(double penalty) {
+    public static double penaltyForOverSpeeding(double penalty1) {
+        penalty1 = 0;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter Speed: ");
         int speed = scanner.nextInt();
 
         int overSpeed = speed - 50;
-        penalty = 0;
 
         if (overSpeed < 0) {
-            System.out.println("You're a good driver!");
+            System.out.println("Not over speeding!");
         } else if ((overSpeed > 0) && (overSpeed <= 10)) {
             System.out.println("You are driving " + overSpeed + " km/h more than allowed. You will get a written warning!");
         } else if (overSpeed <= 20) {
             System.out.println("You are driving " + overSpeed + " km/h more than allowed. You should pay the penalty: 30 Euro");
-            penalty = penalty + 30;
+            penalty1 = penalty1 + 30;
         } else if (overSpeed <= 30) {
             System.out.println("You are driving " + overSpeed + " km/h more than allowed. You should pay the penalty: 50 Euro");
-            penalty = penalty + 50;
+            penalty1 = penalty1 + 50;
         } else if (overSpeed <= 50) {
             System.out.println("You are driving " + overSpeed + " km/h more than allowed. You should pay the penalty: 100 Euro");
-            penalty = penalty + 100;
+            penalty1 = penalty1 + 100;
         } else if (overSpeed <= 100) {
             System.out.println("You are driving " + overSpeed + " km/h more than allowed. You should pay the penalty: 500 Euro");
-            penalty = penalty + 500;
+            penalty1 = penalty1 + 500;
         } else {
             System.out.println("You are driving " + overSpeed + " km/h more than allowed. You should pay the penalty: 1500 Euro");
-            penalty = penalty + 1500;
+            penalty1 = penalty1 + 1500;
         }
+        return penalty1;
+    }
 
+        public static double penaltyForDrinking(double penalty2) {
+
+        System.out.println("-----------------------------------------------");
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter alcohol level: ");
         double alcoholLevel = scanner.nextDouble();
+        penalty2 = 0;
 
         if (alcoholLevel < 0) {
             System.out.println("No alcohol in your system!");
@@ -51,60 +55,60 @@ public class Exercise4 {
             System.out.println("Alcohol level is within tolerance.");
         } else if (alcoholLevel <= 1.0) {
             System.out.println("Your alcohol level is: " + alcoholLevel + "pro/mille. Your penalty is 50 Euro");
-            penalty = penalty + 50;
+            penalty2 = penalty2 + 50;
         } else if (alcoholLevel <= 2.0) {
             System.out.println("Your alcohol level is: " + alcoholLevel + "pro/mille. Your penalty is 100 Euro");
-            penalty = penalty + 100;
+            penalty2 = penalty2 + 100;
         } else {
             System.out.println("Your alcohol level is: " + alcoholLevel + "pro/mille. Your penalty is 2500 Euro");
-            penalty = penalty + 2500;
+            penalty2 = penalty2 + 2500;
         }
-
-        return penalty;
+        return penalty2;
     }
 
-    public static double discount(double penalty) {
-        if (penalty > 0) {
-            System.out.println("Your total penalty is: " + penalty + "Euro");
+    public static double discount( String month) {
+        double penaltyTotal = penaltyForDrinking(2500) + penaltyForOverSpeeding(100);
+        if (penaltyTotal > 0) {
+            System.out.println("Your total penalty is: " + penaltyTotal + "Euro");
 
             Scanner scanner = new Scanner(System.in);
             System.out.println("Enter Month: ");
-            String month = scanner.next();
+            month = scanner.next();
 
             switch (month) {
                 case "January":
                 case "February":
-                    System.out.println("It is " + month + ". No discount possible! Your total penalty is " + penalty + " Euro.");
+                    System.out.println("It is " + month + ". No discount possible! Your total penalty is " + penaltyTotal + " Euro.");
                     break;
                 case "March":
                 case "April":
-                    penalty = penalty * 0.90;
-                    System.out.println("It is " + month + ". Your total penalty is " + penalty + " Euro.");
+                    penaltyTotal = penaltyTotal * 0.90;
+                    System.out.println("It is " + month + ". Your total penalty is " + penaltyTotal + " Euro.");
                     break;
                 case "May":
                 case "June":
-                    penalty = penalty * 0.85;
-                    System.out.println("It is " + month + ". Your total penalty is " + penalty + " Euro.");
+                    penaltyTotal = penaltyTotal * 0.85;
+                    System.out.println("It is " + month + ". Your total penalty is " + penaltyTotal + " Euro.");
                     break;
                 case "July":
                 case "August":
-                    penalty = penalty * 0.80;
-                    System.out.println("It is " + month + ". Your total penalty is " + penalty + " Euro.");
+                    penaltyTotal = penaltyTotal * 0.80;
+                    System.out.println("It is " + month + ". Your total penalty is " + penaltyTotal + " Euro.");
                     break;
                 case "September":
                 case "October":
-                    penalty = penalty * 0.75;
-                    System.out.println("It is " + month + ". Your total penalty is " + penalty + " Euro.");
+                    penaltyTotal = penaltyTotal * 0.75;
+                    System.out.println("It is " + month + ". Your total penalty is " + penaltyTotal + " Euro.");
                     break;
                 case "November":
                 case "December":
-                    penalty = penalty * 0.70;
-                    System.out.println("It is " + month + ". Your total penalty is " + penalty + " Euro.");
+                    penaltyTotal = penaltyTotal * 0.70;
+                    System.out.println("It is " + month + ". Your total penalty is " + penaltyTotal + " Euro.");
                     break;
             }
         } else {
             System.out.println(" Everything's fine!");
         }
-        return penalty;
+        return penaltyTotal;
     }
 }
