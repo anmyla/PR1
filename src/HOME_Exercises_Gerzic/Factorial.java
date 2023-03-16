a8df40491192e3df9bfb3c2b224a460b739f22ff
anmyla <myla.ariaga@gmail.com>
2023-03-16 10:23:00 +0100

    10:22 16032023

--- src/HOME_Exercises_Gerzic/Factorial.java
+++ src/HOME_Exercises_Gerzic/Factorial.java
@@ -8,12 +8,10 @@ public class Factorial {
         System.out.println("Enter a number: ");
         int number = input.nextInt();
 
-        int factorial = 0;
-        for (int i = number; i > 0; i--){
-            factorial = number * i;
-            System.out.println(factorial);
+        int factorial = number;
+        for (int i = number - 1; i > 0; i--) {
+            factorial = factorial * i;
         }
-
+        System.out.println("The factorial of the number " + number + " is: " + factorial);
     }
 }
-//program not yet completely finished. it runs but gives the wrong output.
--- src/HOME_Exercises_Gerzic/Programmauswahlmenue.java
+++ src/HOME_Exercises_Gerzic/Programmauswahlmenue.java
@@ -1,81 +1,154 @@
 package HOME_Exercises_Gerzic;
 
+import java.util.Locale;
 import java.util.Scanner;
-
-import static java.lang.System.out;
-
-public class Programmauswahlmenue {
+public class Programmauswahlmenue { // UGLY program but it works!!!
     public static void main(String[] args) {
+        int choice = 0;
+        System.out.println("Menu: \n" +
+                "1. Reverse String Output \n" +
+                "2. Odd or Even Number \n" +
+                "3. Count the Vowels \n" +
+                "4. Calculate the Factorial \n" +
+                "5. Terminate the Program");
+        do {
+            Scanner again = new Scanner(System.in);
+            System.out.println("Enter your choice: " );
+            choice = again.nextInt();
+        } while (choice < 1 || choice > 5);
 
-        oddOrEven(59);
-        out.println();
-    reverse();
-        out.println();
-    countTheVowels();
+        do {
+            switch (choice) {
+                case 1:
+                    reversedWord();
+                    System.out.println("Menu: \n" +
+                            "1. Reverse String Output \n" +
+                            "2. Odd or Even Number \n" +
+                            "3. Count the Vowels \n" +
+                            "4. Calculate the Factorial \n" +
+                            "5. Terminate the Program");
 
+                    Scanner again = new Scanner(System.in);
+                    again = new Scanner(System.in);
+                    System.out.println("Please enter the program you want to run: ");
+                    choice = again.nextInt();
+                    break;
+
+                case 2:
+                    oddOrEven();
+                    System.out.println("Menu: \n" +
+                            "1. Reverse String Output \n" +
+                            "2. Odd or Even Number \n" +
+                            "3. Count the Vowels \n" +
+                            "4. Calculate the Factorial \n" +
+                            "5. Terminate the Program");
+
+                    again = new Scanner(System.in);
+                    System.out.println("Please enter the program you want to run: ");
+                    choice = again.nextInt();
+                    break;
+
+                case 3:
+                    countTheVowels();
+                    System.out.println("Menu: \n" +
+                            "1. Reverse String Output \n" +
+                            "2. Odd or Even Number \n" +
+                            "3. Count the Vowels \n" +
+                            "4. Calculate the Factorial \n" +
+                            "5. Terminate the Program");
+
+                    again = new Scanner(System.in);
+                    System.out.println("Please enter the program you want to run: ");
+                    choice = again.nextInt();
+                    break;
+
+                case 4:
+                    factorial();
+                    System.out.println("Menu: \n" +
+                            "1. Reverse String Output \n" +
+                            "2. Odd or Even Number \n" +
+                            "3. Count the Vowels \n" +
+                            "4. Calculate the Factorial \n" +
+                            "5. Terminate the Program");
+
+                    again = new Scanner(System.in);
+                    System.out.println("Please enter the program you want to run: ");
+                    choice = again.nextInt();
+                    break;
+            }
+        } while (choice < 5);
+        System.out.println("Thanks for banking with us! Goodbye!");
     }
-//    public static void reversedWord() {
-//        Scanner scanner1 = new Scanner(System.in);
-//        out.println("Enter a word: ");
-//        String word = scanner1.next();
-//
-//        String reversedWord = "";
-//        for (int i = word.length() - 1; i >= 0; i--) {
-//            reversedWord = reversedWord + reversedWord.charAt(i);
-//        }
-//        out.println(reversedWord);
-//    }
 
-    public static void reverse() {
+
+    private static int getChoice(int choice) {
+        System.out.println("Menu: \n" +
+                "1. Reverse String Output \n" +
+                "2. Odd or Even Number \n" +
+                "3. Count the Vowels \n" +
+                "4. Calculate the Factorial \n" +
+                "5. Terminate the Program");
+
+        Scanner again = new Scanner(System.in);
+        System.out.println("Please enter the program you want to run: ");
+        choice = again.nextInt();
+        return choice;
+    }
+
+    public static void reversedWord() {
         Scanner scanner1 = new Scanner(System.in);
-        System.out.println("Enter a word: ");
-        String word = scanner1.nextLine();
+        System.out.println("Enter a word to reverse: ");
+        String word = scanner1.next();
 
-        //1. save the value of the String "word" to a char[]
-        char[] stringToCharArray = word.toCharArray();
-
-        //2. create 2 int variables to represent the beginning and end of the word
-        int fromStartOfWord;
-        int fromEndOfWord = stringToCharArray.length -1;
-
-        //3. using the 2 int variables, create the for loop to reverse the letters in the stringToCharArray
-        for (fromStartOfWord = 0; fromStartOfWord < fromEndOfWord; fromStartOfWord++, fromEndOfWord--) {
-            char reversedWord = stringToCharArray[fromStartOfWord];
-            stringToCharArray[fromStartOfWord] = stringToCharArray[fromEndOfWord];
-            stringToCharArray[fromEndOfWord] = reversedWord;
+        String reversedWord = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            char letter = word.charAt(i);
+            reversedWord = reversedWord + letter;
         }
-        out.println("The word in reverse is: ");
-        for (char reversedWord : stringToCharArray)
-            System.out.print(reversedWord);
+        System.out.println("The word \"" + word + "\" when reversed is: " + reversedWord);
     }
 
-    public static boolean oddOrEven(double number) {
-        boolean even = false;
-        if(number % 2 == 0) {
+
+    public static void oddOrEven() {
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Enter a number to find out if it is odd or even: ");
+        int number = scanner2.nextInt();
+
+        boolean even;
+        if (number % 2 == 0) {
             even = true;
-            System.out.println("The number: "+ number +  " is even");
+            System.out.println("The number: " + number + " is even");
+        } else {
+            System.out.println("The number: " + number + " is odd");
         }
-        else {
-            even= false;
-            out.println("The number: " + number +  " is odd");
-        }
-        return even;
     }
 
     public static void countTheVowels() {
-
-        Scanner scanner2 = new Scanner(System.in);
-        out.println("Enter a phrase: ");
-        String phrase = scanner2.nextLine();
+        Scanner scanner3 = new Scanner(System.in);
+        System.out.println("Enter a phrase: ");
+        String phrase = scanner3.nextLine();
 
         int vowelCounter = 0;
         char letterChecker;
-        for (int i=0 ; i<phrase.length(); i++){
+        phrase = phrase.toLowerCase();
+        for (int i = 0; i < phrase.length(); i++) {
             letterChecker = phrase.charAt(i);
-            if(letterChecker == 'a'|| letterChecker == 'e'|| letterChecker == 'i' ||letterChecker == 'o' ||letterChecker == 'u'||letterChecker == ' '){
+            if (letterChecker == 'a' || letterChecker == 'e' || letterChecker == 'i' || letterChecker == 'o' || letterChecker == 'u') {
                 vowelCounter++;
             }
         }
-        System.out.println("Number of vowels in the given sentence is "+ vowelCounter);
+        System.out.println("Number of vowels in the given phrase is " + vowelCounter);
+    }
+
+    public static void factorial() {
+        Scanner scanner4 = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = scanner4.nextInt();
+
+        int factorial = number;
+        for (int i = number - 1; i > 0; i--) {
+            factorial = factorial * i;
+        }
+        System.out.println("The factorial of the number " + number + " is: " + factorial);
     }
 }
