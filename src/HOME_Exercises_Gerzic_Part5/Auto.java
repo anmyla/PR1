0848f8f28d456b1fe100817088052fab3b2b6e42
anmyla <myla.ariaga@gmail.com>
2023-03-23 20:06:12 +0100

    20:00,  23.march

--- src/HOME_Exercises_Gerzic_Part5/Auto.java
+++ src/HOME_Exercises_Gerzic_Part5/Auto.java
@@ -1,12 +1,14 @@
 package HOME_Exercises_Gerzic_Part5;
 
 public class Auto {
-    String make = "";
-    String model = "";
-    int year;
+     public String make = "";
+    public String model = "";
+    public int year;
     double speed;
 
-    public static void kindOfCar(String make, String model, int year) {
+
+    public static String kindOfCar(String make, String model, int year) {
+        return "Make: " + make + " Model: " + model + " Year: " +year;
     }
 
     public static String getMake(){
@@ -23,37 +25,17 @@ public class Auto {
 
     public static void setModel(String model){}
 
-    public static dou
-}
+    public static double stepOnGas(double speed, double speedUpBy) {
+        speed = speed * speedUpBy;
+        return speed;
+    }
 
-//The class should have the following methods:
-//- a constructor that specifies the make, model, and year of the vehicle.
-//specified.
-//- getters and setters (methods) for each attribute.
-//- a method called "gasGeben(double geschw)", which takes a double as a
-//parameter and increases the speed of the car by this value.
-//- a method named "braking(double speed)", which takes a double as a parameter and increases the speed of the car by this value.
-//parameter and decreases the speed of the car by this value.
-//decreased.
-//- a method called "toString()", which returns a string representation of the car
-//in the following format -> example output:
-//o "Make: BMW, Model: G30, Year: 2020, Speed: 210".
-//Create a main class called "AutoTest" that creates two instances of the class
-//"Auto", sets its attributes and tests its methods by calling them and outputting the results.
-//and outputting the results.
-//For example, the main class might create an "Auto" object named "auto1" with
-//the following attributes:
-//- Make: "Toyota"
-//- Model: "Camry
-//- Year: 2018
-//- Speed: 0
-//Test these methods of the class Auto in the main method in the class
-//AtuoTest. (This is only for running the program and testing the functions
-//of the class Recipe)
-//For example, you could use the method "gasGeben()" for "auto1" with a parameter of
-//100.0 and the method "toString()" for "auto1" could be called to output the result in the console.
-//the console. After that the car should be braked by 60.0 Km/h, because a deer
-//PR1 exercises
-//6
-//jumps onto the road. At the same time "auto2" could accelerate on the highway around 260 Km/h
-//accelerate.
+    public static double stepOnBreak (double speed, double slowDownBy){
+        speed = speed * slowDownBy;
+        return speed;
+    }
+
+    public String toString() {
+        return "Make: " + make + " Model: " + model + " Year: " +year;
+    }
+}
