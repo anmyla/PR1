65b0c38b2ffb40b70cb06c0699d7de77de8dd4da
anmyla <myla.ariaga@gmail.com>
2023-04-02 20:54:07 +0200

    tamagotchi edits

--- src/Exer3_Gerzic_Objects_Construct_Parameters_GetSet/Tamagotchi.java
+++ src/Exer3_Gerzic_Objects_Construct_Parameters_GetSet/Tamagotchi.java
@@ -13,64 +13,88 @@ public class Tamagotchi {
         this.foodStore = foodStore;
     }
 
-    public int feedTamagochi(int food){
+    public String feedTamagotchi(int food) {
         this.foodStore = this.foodStore + food;
-        return foodStore;
+        return "I fed my Tamagotchi with : " +food+ " food";
     }
 
-    public void direction (String direction) {
+    public String findTamagotchi(int x, int y) {
+        this.x = x;
+        this.y = y;
+
+        return "My Tamagotchi is at position Y: " + y + " and X: " + x;
+    }
+
+    public void direction(String direction) {
+        direction.toLowerCase();
         if (foodStore < 1) {
             return;
         }
-        if (direction.equals("down")){
+        if (direction.equals("down")) {
             y--;
-        }
-        else if (direction.equals("up")){
+        } else if (direction.equals("up")) {
             y++;
-        }
-        else if (direction.equals("right")){
+        } else if (direction.equals("right")) {
             x++;
-        }
-        else if (direction.equals("left")){
+        } else if (direction.equals("left")) {
             x--;
         }
         foodStore--;
     }
 
-    public void forLoop() {
-        System.out.println("For-Loop Approach--------------------------");
+    public void forLoopMethod(int y, int x, int foodStore) {
+        System.out.println('\n'+"For-Loop Approach--------------------------");
+        System.out.print("[ X: " + x + " ] [food left: " + foodStore + " ]");
         for (int j = x; j > 0; j--) {
-            direction("left");
-            System.out.print("| X: " + x + " food left: " + foodStore);
+            if (foodStore > 1) {
+                x--;
+                foodStore--;
+            } else {
+                return;
+            }
+            System.out.print("[ X: " + x + " ] [food left: " + foodStore + " ]");
         }
         System.out.println();
+        System.out.print("[ Y: " + y + " ] [food left: " + foodStore + " ]");
         for (int i = y; i > 0; i--) {
-            direction("down");
-            System.out.print("| Y: " + y + " food left: " + foodStore);
+            if (foodStore > 1) {
+                y--;
+                foodStore--;
+
+            } else {
+                return;
+            }
+            System.out.print("[ Y: " + y + " ] [food left: " + foodStore + " ]");
         }
         System.out.println();
-        System.out.println("| X: " + x + " | Y: " + y + " | food left : " + foodStore);
+        System.out.println("Position:  | X: " + x + " | Y: " + y + " | food left : " + foodStore);
+
+        if (x == 0 && y == 0) {
+            System.out.println("Goal reached successfully!");
+        } else {
+            System.out.println("Not enough food to reach goal!");
+        }
     }
 
     public void whileLoop(int y, int x, int foodStore) {
         System.out.println('\n' + "While-Loop Approach---------------");
         while (x != 0 && y != 0) {
+            System.out.println(" | x: " + x + " | y:" + y + " | food left: " + foodStore);
+            if (x >= 0 && foodStore != 0) {
+                direction("down");
+                foodStore = foodStore - 1;
+            }
+            if (y >= 0 && foodStore != 0) {
+                direction("left");
+                foodStore = foodStore - 1;
+            }
+            x--;
+            y--;
+        }
         System.out.println(" | x: " + x + " | y:" + y + " | food left: " + foodStore);
-        if (x >= 0 && foodStore != 0) {
-            direction("down");
-            foodStore = foodStore -1;
-        }
-        if (y >= 0 && foodStore != 0) {
-            direction("left");
-            foodStore = foodStore -1;
-        }
-        x--;
-        y--;
     }
-        System.out.println(" | x: " + x + " | y:" + y + " | food left: " + foodStore);
-}
 
-    public char[][] printBoard () {
+    public char[][] printBoard() {
         char[][] position1 = new char[10][10];
         char holder = ' ';
 
--- src/Exer3_Gerzic_Objects_Construct_Parameters_GetSet/TamagotchiMain.java
+++ src/Exer3_Gerzic_Objects_Construct_Parameters_GetSet/TamagotchiMain.java
@@ -2,15 +2,45 @@ package Exer3_Gerzic_Objects_Construct_Parameters_GetSet;
 
 public class TamagotchiMain {
     public static void main(String[] args) {
+
+        System.out.println("Testing some methods with own parameters---------");
         Tamagotchi pliki = new Tamagotchi(4, 4, 0);
-        System.out.println("Fed my Tamagotchi with : " +pliki.feedTamagochi(10) + " food" + '\n');
+        System.out.println(pliki.findTamagotchi(6,7));
+        System.out.println(pliki.feedTamagotchi(10) + '\n');
 
-        pliki.forLoop(); //takes previously initialized values
+        //using previously initialized variables and calling a method "direction()2, to bring tamagotchi to position x:0, y:0
+        System.out.println("Using method direction() ");
+        System.out.print("[ X: " + pliki.x + " ] [food left: " + pliki.foodStore + " ]");
+        for (int j = pliki.x; j > 0; j--) {
+            pliki.direction("left");
+            if (pliki.foodStore > 0) {
+                System.out.print("[ X: " + pliki.x + " ] [food left: " + pliki.foodStore + " ]");
+            }
+            else {
+                break;
+            }
+            }
+        System.out.println();
+        System.out.print("[ Y: " + pliki.y + " ] [food left: " + pliki.foodStore + " ]");
+        for (int i = pliki.y; i > 0; i--) {
+            pliki.direction("down");
+            if (pliki.foodStore>0) {
+            System.out.print("[ Y: " + pliki.y + " ] [food left: " + pliki.foodStore + " ]");
+            }
+            else {
+                break;
+            }
+        }
+        System.out.println();
+        System.out.println("Position:  | X: " + pliki.x + " | Y: " + pliki.y + " | food left : " + pliki.foodStore);
 
-        pliki.whileLoop(6,6,26); //use new  values from these parameters
+        if (pliki.x == 0 && pliki.y == 0) {
+            System.out.println("Goal reached successfully!");
+        } else {
+            System.out.println("Not enough food to reach goal!");
+        }
 
-
-//----------------------------1st TRY working-----------------------------------------
-
+        pliki.forLoopMethod(2,2, 5);
+        pliki.whileLoop(6,6,26); //use new  values from own parameters
     }
 }
